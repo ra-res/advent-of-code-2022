a233fbf7641b4edf7ffcc1d424edb55094344a50
ra-res <ra-res@users.noreply.github.com>
2022-12-19 18:24:18 +0000

    feat: day 5

--- 1/index.scala
+++ 1/index.scala
@@ -5,8 +5,8 @@ import scala.io.Source.fromFile
 import scala.util.Using
 
 @main def main() = {
-    // val lines = Using(fromFile("small.txt"))(_.mkString).get
-    val lines = Using(fromFile("input.txt"))(_.mkString).get
+    // val lines: String = Source.fromFile("small.txt").mkString
+    val lines: String = Source.fromFile("input.txt").mkString
     val calories = lines.split("\n\n").map(_.split("\n").map(_.toInt))
     val top3 = calories.map(_.sum).sorted.reverse.take(3)
     println(top3(0)) // 69795
---
+++ 5/index.scala
@@ -0,0 +1,20 @@
+import scala.io.Source
+
+@main def main() = {
+//   val lines = Source.fromFile("small.txt").mkString.split("\n\n")
+  val lines = Source.fromFile("input.txt").mkString.split("\n\n")
+  val lsts = lines(0).split("\n").reverse.toList.tail.map(_.tail.grouped(4).map(_.head).toList)
+  val stacks = (for (i <- 0 until lsts(0).length) yield lsts.map(_(i)).filter(_ != ' ')).toArray
+  val re = """move (\d+) from (\d+) to (\d+)""".r
+  val ins = lines(1).split("\n").map({ case re(amount, from, to) => (amount.toInt, from.toInt - 1, to.toInt - 1) }).toList
+
+  ins.foreach(in => {
+    val amount = in(0)
+    val from = in(1)
+    val to = in(2)
+    stacks(to) = stacks(to) ++ stacks(from).takeRight(amount) // .reverse
+    stacks(from) = stacks(from).dropRight(amount)
+  })
+
+  println(stacks.map(_.last).mkString)
+}
